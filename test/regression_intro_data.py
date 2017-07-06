16d3a9d71cdd5ad07f32ba5941befe8ac504760e
wuhao <wu_hao1314520@126.com>
2017-07-06 10:11:27 +0800

    wuhao2017/07/06

--- test/regression_intro_data.py
+++ test/regression_intro_data.py
@@ -2,7 +2,10 @@
 __author__ = 'wuhao'
 __date__ = '2017/7/4 18:38'
 import pandas as pd
-import Quandl
+import quandl
 
-df = Quandl.get('WIKI/GOOGL')
-print(df)
+# df = quandl.get('WIKI/GOOGL')
+
+# mydata = quandl.get("FRED/GDP")
+mydata1 = quandl.get("EIA/PET_RWTC_D")
+print(mydata1)
